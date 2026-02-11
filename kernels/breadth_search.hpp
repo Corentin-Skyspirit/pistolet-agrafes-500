a0a0ce4010b1b5c6c897be904a0acc7d1be987f9
Pierre BAUMANN <pierre.baumann.n71@gmail.com>
2026-02-11 17:24:31 +0100

    Fixed compilation issues with gen_graph

--- kernels/breadth_search.hpp
+++ kernels/breadth_search.hpp
@@ -1,4 +1,14 @@
 #ifndef BREADTH_SEARCH_H
 #define BREADTH_SEARCH_H
 
+#include "gen_graph.hpp"
+#include <cstdint>
+
+typedef struct {
+	uint64_t* parent_array;
+	double teps;
+} bfs_result;
+
+bfs_result bfs(graph& g);
+
 #endif
--- kernels/gen_graph.hpp
+++ kernels/gen_graph.hpp
@@ -1,14 +1,17 @@
 #ifndef GEN_GRAPH_H
 #define GEN_GRAPH_H
 
-typedef struct {
+#include "../generator/generator.hpp"
+#include <cstdint>
+
+typedef struct graph graph;
+struct graph {
 	uint64_t* slicing_idx;
 	uint64_t* neighbors;
 	float* weight;
 	double time_s;
 
 	graph from_edge_list(edge_list input_list);
-
-} graph;
+};
 
 #endif
