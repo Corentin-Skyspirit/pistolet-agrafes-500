60f4538b83ba7b43dc13e21f546b9e2a061c1589
Corentin Arnaud <corentin.arnaud45@gmail.com>
2026-02-12 17:52:22 +0100

    neighbors list CSR up !

--- kernels/gen_graph.cpp
+++ kernels/gen_graph.cpp
@@ -14,20 +14,35 @@ graph from_edge_list(edge_list input_list) {
 	g.slicing_idx = (int64_t*)malloc((max_node + 1) * sizeof(int64_t));
 	int64_t cpt = 0;
 	int64_t cpt_idx = 1;
-	g.slicing_idx[0] = 0;
+	g.slicing_idx[0] = cpt;
 	for (int64_t node = 0; node <= max_node; node++) {
 		for (int64_t i = 0; i < input_list.length; i++) {
 			if (input_list.edges[i].v0 != input_list.edges[i].v1) {
 
+				int64_t neighbor = -1;
+
 				if (input_list.edges[i].v0 == node) {
-					g.neighbors[cpt] = input_list.edges[i].v1;
-					g.weights[cpt] = input_list.weights[i];
-					cpt++;
+					neighbor = input_list.edges[i].v1;
 				}
-				if (input_list.edges[i].v1 == node) {
-					g.neighbors[cpt] = input_list.edges[i].v0;
-					g.weights[cpt] = input_list.weights[i];
-					cpt++;
+				else if (input_list.edges[i].v1 == node) {
+					neighbor = input_list.edges[i].v0;
+				}
+
+				if (neighbor != -1) {
+					bool found = false;
+					for (int64_t j = g.slicing_idx[cpt_idx - 1]; j < cpt; j++) {
+						if (g.neighbors[j] == neighbor) {
+							g.weights[j] = input_list.weights[i];
+							found = true;
+							break;
+						}
+					}
+
+					if (!found) {
+						g.neighbors[cpt] = neighbor;
+						g.weights[cpt] = input_list.weights[i];
+						cpt++;
+					}
 				}
 			}
 		}
